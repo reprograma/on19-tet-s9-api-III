86b510590c70bd22e5f7ea5ecb9c69735409620a
Shirlene Teles <shiteles@gmail.com>
2022-10-08 20:36:13 -0300

    exemplos put, patch e delete

--- exercicios/para-sala/index.js
+++ exercicios/para-sala/index.js
@@ -1,23 +1,7 @@
-<<<<<<< HEAD
-const express = require ('express')
-const app = express()
-const listaDeTarefas = require("./model/todo-list.json")
-const port = 3003
-
-app.use(express.json())
-
-//[x] criaremos uma rota utilizando o método PUT que atualiza todos os dados da lista de tarefas e caso não encontre, cria o item.
-
-app.put("/tarefas/:id",(req,res)=>{
-    const IDTarefa = req.params.id
-    const tarefaAtualizada = req.body
-
-    const temTarefa = listaDeTarefas.find(tarefa => tarefa.id == IDTarefa)
-=======
 const express = require('express')
 const app = express()
 const listaDeTarefas = require("./model/todo-list.json")
-const port = 3000
+const port = 3003
 
 app.use(express.json())
 
@@ -37,7 +21,6 @@ app.put("/tarefas/:id",(req, res)=>{
     const tarefaAtualizada = req.body
 
     const temTarefa = listaDeTarefas.find(tarefa => tarefa.id == IDtarefa)
->>>>>>> 41e400dcc41e643e85f1a9c81f3a8353967d811e
 
     if(temTarefa){
         return res.status(200).json(tarefaAtualizada)
@@ -45,24 +28,10 @@ app.put("/tarefas/:id",(req, res)=>{
 
     listaDeTarefas.push(tarefaAtualizada)
 
-<<<<<<< HEAD
-    return res.status(201).json(tarefaAtualizada) // status code : ACCEPTED
-})
-
-
-//[ ] vamos atualizar um registro na lista de tarefas utilizando o método PATCH
-
-app.patch("/tarefas/:id",(req,res)=>{
-    const IDTarefa = req.params.id
-    const novosCampos = req.body
-
-    const existeTarefa = listaDeTarefas.find(tarefa => tarefa.id == IDTarefa)
-
-    if(existeTarefa){
-=======
     return res.status(201).json(tarefaAtualizada) 
     
 })
+
 
 //TODO: [X] vamos atualizar um registro na lista de tarefas utilizando o método PATCH - DONE
 
@@ -73,16 +42,10 @@ app.patch("/tarefas/:id",(req, res)=>{
     const existeTarefa = listaDeTarefas.find(tarefa => tarefa.id == IDtarefa)
 
     if(existeTarefa) {
->>>>>>> 41e400dcc41e643e85f1a9c81f3a8353967d811e
         const tarefaAtualizada = {
             ...existeTarefa,
             ...novosCampos
         }
-<<<<<<< HEAD
-        return res.status(200).json(tarefaAtualizada)
-    }
-    res.status(404).json({
-=======
 
         return res.status(200).json(tarefaAtualizada)
     }
@@ -90,6 +53,7 @@ app.patch("/tarefas/:id",(req, res)=>{
 })
 
 //TODO: [X] apagaremos uma tarefa da lista utilizando o método DELETE. - DONE
+
 app.delete("/tarefas/:id",(req, res)=>{
     const IDtarefa = req.params.id
 
@@ -107,32 +71,9 @@ app.delete("/tarefas/:id",(req, res)=>{
     }
 
     return res.status(404).json({
->>>>>>> 41e400dcc41e643e85f1a9c81f3a8353967d811e
         message:"Tarefa não foi encontrada"
     })
 })
-
-<<<<<<< HEAD
-//[ ] apagaremos uma tarefa da lista utilizando o método DELETE. 
-
-app.delete("/tarefas/:id",(req,res)=>{
-    const IDTarefa = req.params.id
-
-    const existeTarefa = listaDeTarefas.find((tarefa)=> tarefa.id == IDTarefa)
-
-    if(existeTarefa){
-        listaDeTarefas.map((tarefa,index)=>{
-            if(tarefa.id == IDTarefa){
-                return listaDeTarefas.splice(index,1)
-            }
-        })
-        return res.status(200).json(listaDeTarefas)
-    }
-    return res.status(404).json({
-        message:"Tarefa não foi encontarada"
-    })
-})
-
 
 /* 
 Segunda opção que fiz para deletar um item
@@ -151,10 +92,5 @@ app.delete("/tarefas/:id",(req,res)=>{
 */
 
 app.listen(port,()=>{
-    console.log(`Api esta rodando na porta ${port}`);
-=======
-
-app.listen(port,()=>{
-    console.log(`Api está rodando na porta ${port}`)
->>>>>>> 41e400dcc41e643e85f1a9c81f3a8353967d811e
+    console.log(`Api esta rodando na porta ${port}`)
 })
