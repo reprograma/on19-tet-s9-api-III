9df4794449af355f7874b3d8606302d26407ccef
Lilia Oliveira <liliatatyany@gmail.com>
2022-10-10 17:38:00 -0300

    exercicio semana 9

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -5,18 +5,23 @@ const port = 3000
 
 app.use(express.json())
 
+ // Usei o GET apenas para retornar a lista original de usuários
+app.get("/usuarios", (req, res) => {
+    res.json(listaUsuarios)
+})
+
 
 
 /**
  * - Utilizando os conhecimentos compartilhados na aula crie uma API restful com ExpressJS que entregue as seguintes rotas:
 
-- [ ] Uma rota que atualiza apenas o endereço do usuário
-- [ ] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
+
+
 - [ ] Usar corretamente os retornos com os respectivos status codes!
 
  */
 
-//- [ ] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista
+//- [ ] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista DONE 
 
 app.put("/usuarios/:id", (req, res) => {
     const IDusuario = req.params.id
@@ -35,7 +40,45 @@ app.put("/usuarios/:id", (req, res) => {
 })
 
 
+//- [ ] Uma rota que atualiza apenas o endereço do usuário DONE 
 
+app.patch("/usuarios/:id",(req, res)=>{
+    const IDusuario = req.params.id
+    const updateUsuario = req.body
+
+    const usuario = listaUsuarios.find(usuario => usuario.id == IDusuario)
+
+    if(usuario) {
+        const usuarioAtualizado = {
+            ...usuario,
+            ...updateUsuario
+        }
+       
+        return res.status(200).json(usuarioAtualizado)
+    }
+    return res.status(404).json({message:"tarefa não foi encontrada"})
+})
+
+//- [ ] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
+app.delete("/usuarios/:id",(req, res)=>{
+    const IDusuario = req.params.id
+
+    const deletarUsuario = listaUsuarios.find((usuario) => usuario.id == IDusuario)
+   
+    if(deletarUsuario){
+        listaUsuarios.map((usuario, index)=>{
+            if(usuario.id == IDusuario){
+                return listaUsuarios.splice(index,1)
+            }
+        })
+
+        return res.status(200).json(listaUsuarios)
+    }
+
+    return res.status(404).json({
+        message:"Tarefa não foi encontrada"
+    })
+})
 
 app.listen(port,()=>{
     console.log(`Api está rodando na porta ${port}`)
