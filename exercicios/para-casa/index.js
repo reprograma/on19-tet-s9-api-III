25fed827225e487a097cd230aa1d3e98c1faf39f
Shirlene Teles <shiteles@gmail.com>
2022-10-10 22:03:53 -0300

    atualização atividade

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -5,7 +5,7 @@ const port = 3030
 
 app.use(express.json())
 
-//[ ] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista
+//[x] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista
 
 app.put("/user/:id",(req,res)=>{
     const IDUser = req.params.id
@@ -14,14 +14,19 @@ app.put("/user/:id",(req,res)=>{
     const validUser = userList.find(user => user.id == IDUser)
 
     if(validUser){
+        userList.map((user,index)=>{
+            if(user.id == IDUser){
+                return userList[index] = updatedUser
+            }
+        })
         return res.status(200).json(updatedUser)
     }
     userList.push(updatedUser)
 
-    return res.status(201).json(updatedUser)
+    return res.status(201).json(updatedUser)  
 })
 
-//[ ] Uma rota que atualiza apenas o endereço do usuário
+//[x] Uma rota que atualiza apenas o endereço do usuário
 
 app.patch("/user/:id",(req,res)=>{
     const IDUser = req.params.id
@@ -30,16 +35,21 @@ app.patch("/user/:id",(req,res)=>{
     const validUser = userList.find(user => user.id == IDUser)
 
     if(validUser){
-        validUser.address = newAddress
-        
-        return res.status(200).json(validUser)
+        const updatedUser = {
+            ...validUser,
+            ...newAddress
+        }
+         userList.map((user, index)=>{
+            if(user.id == IDUser){
+                return userList[index] = updatedUser
+            }
+        })
+        return res.status(200).json(updatedUser)
     }
-    res.status(404).json({
-        message:"Usuário não foi encontrado"
-    })
+    return res.status(404).json({message:"Usuário não foi encontrado"})
 })
 
-//[ ] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
+//[x] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
 
 app.delete("/user/:id",(req,res)=>{
     const IDUser = req.params.id
@@ -47,9 +57,9 @@ app.delete("/user/:id",(req,res)=>{
     const validUser = userList.find((user)=> user.id == IDUser)
 
     if(validUser){
-        userList.map((user,i)=>{
+        userList.map((user,index)=>{
             if(user.id == IDUser){
-                return userList.splice(i,1)
+                return userList.splice(index,1)
             }
         })
         return res.status(200).json(userList)
