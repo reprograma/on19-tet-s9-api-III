029684b0e14a1b27ce4e6427923c7228fc03fb27
elisangelamanuel <elisangelasofs@gmail.com>
2022-10-13 19:36:18 -0300

    Format Document

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -4,22 +4,22 @@ const port = 3000
 const userList = require("./model/usuarios.json")
 
 app.use(express.json())
- 
+
 app.put("/users/:id", (req, res) => {
     const userId = Number(req.params.id)
     const userData = req.body
-    
+
     const userUpdate = userList.findIndex(user => user.id === userId)
-   
-    if(userList.find((user) => user.id === userId)) {
+
+    if (userList.find((user) => user.id === userId)) {
         userList.splice(userUpdate, 1, userData)
         return res.status(200).json(userData)
     }
 
     userList.push(userData)
     return res.status(201).json(userData)
-    
-    
+
+
 })
 
 app.patch("/users/:id", (req, res) => {
@@ -27,30 +27,30 @@ app.patch("/users/:id", (req, res) => {
     const newAdress = req.body
 
     const userIndex = userList.findIndex(user => user.id === id)
-    
-    if(userList.find((user) => user.id === id)) {
-    const userFinded = userList[userIndex]
-    
-    const adressUpdate = {
-        ...userFinded,
-        ...newAdress
-    }
 
-    userList.splice(userIndex, 1, adressUpdate)
-    return res.status(200).json(adressUpdate)
+    if (userList.find((user) => user.id === id)) {
+        const userFinded = userList[userIndex]
+
+        const adressUpdate = {
+            ...userFinded,
+            ...newAdress
+        }
+
+        userList.splice(userIndex, 1, adressUpdate)
+        return res.status(200).json(adressUpdate)
     }
     return res.status(404).json({
-        message: "Bad request"
+        message: "Not Found"
     })
 })
 
 app.delete("/users/:id", (req, res) => {
     const idForDelete = Number(req.params.id)
     const findUser = userList.find((user) => user.id === idForDelete)
-   
-    if(findUser){
-        userList.map((user, index)=>{
-            if(user.id === idForDelete){
+
+    if (findUser) {
+        userList.map((user, index) => {
+            if (user.id === idForDelete) {
                 return userList.splice(index, 1)
             }
         })
@@ -59,10 +59,10 @@ app.delete("/users/:id", (req, res) => {
     }
 
     return res.status(404).json({
-        message:"Bad request"
+        message: "Not Found"
     })
 })
 
- app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Listening on port ${port}`);
- })
+})
