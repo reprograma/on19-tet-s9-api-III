0ebf5a5595a36db340fda1b726dc62ddaf84bb5f
Mikaela Clotilde <mikaelacs29@gmail.com>
2022-10-13 01:57:18 -0300

    Respostas - etapa 2

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const app = express()
+const listaUsuarios = require("./model/usuarios.json")
+const port = 3030
+
+app.use(express.json())
+
+//fazer: 200 - OK; 201 - CREATED; 202 - ACCEPTED; 204 - NO CONTENT; 404 - NOT FOUND.
+
+app.put("/usuarios/:id",(req, res)=>{
+    const IDusuario = req.params.id
+    const cadastroAtualizado = req.body
+
+    const usuarioExiste = listaUsuarios.find(usuario => usuario.id == IDusuario)
+    if(usuarioExiste){
+        listaUsuarios.map((usuario, index)=>{
+            if(usuario.id == IDusuario){
+                return listaUsuarios[index] = cadastroAtualizado
+            }
+        })
+        return res.status(200).json(cadastroAtualizado)
+    }
+    listaUsuarios.push(cadastroAtualizado)
+
+    return res.status(201).json(listaUsuarios) 
+
+})
+
+app.patch("/usuarios/:id",(req, res)=>{
+    const IDusuario = req.params.id
+    const atualizarEndereco = req.body
+
+    const usuarioExiste = listaUsuarios.find(usuario => usuario.id == IDusuario)
+
+    if(usuarioExiste) {
+        const novoEndereco = {
+            ...usuarioExiste,
+            ...atualizarEndereco
+        }
+
+        listaUsuarios.map((usuario, index)=>{
+            if(usuario.id == IDusuario){
+                return listaUsuarios[index] = novoEndereco
+            }
+        })
+        return res.status(200).json(novoEndereco)
+    }
+    return res.status(404).json()
+})
+
+app.delete("/usuarios/:id",(req, res)=>{
+    const IDusuario = req.params.id
+
+    const usuarioExiste = listaUsuarios.find(usuario => usuario.id == IDusuario)
+     if(usuarioExiste){
+        listaUsuarios.map((usuario, index)=>{
+            if(usuario.id == IDusuario){
+                return listaUsuarios.splice(index,1)
+            }
+        })
+
+        return res.status(200).json(listaUsuarios)
+    }
+
+    return res.status(404).json({
+        message:"Usuário não existe"
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`Aplicação está batendo na porta ${port}`)
+})
