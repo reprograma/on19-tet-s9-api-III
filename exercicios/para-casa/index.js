cbb9f81ba54c1724f1f587f749f384aeda996825
Bianca Trindade <biancatrindade.mkt@gmail.com>
2022-10-14 21:30:29 -0300

    Resolução exercício de casa

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,80 +1,76 @@
 const express = require("express")
 const app = express()
-const dataList = require("./model/usuarios.json")
+const userList = require("./model/usuarios.json")
 const port = 3000 
 
 app.use(express.json())
 
 // [x] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista
 //PUT
-app.put("/data/:id", (req, res) =>{
+app.put("/users/:id", (req, res) =>{
     const idUser = req.params.id
     const updatedUser = req.body
 
-    const thereIsTheUser = dataList.find(user => user.id == idUser)
+    const thereIsTheUser = userList.find(user => user.id == idUser)
 
     if(thereIsTheUser){
-        dataList.map((user, index) =>{
+        userList.map((user, index) =>{
             if(user.id == idUser){
-                return dataList[index] = updatedUser
+                return userList[index] = updatedUser
             }
         })
-        return res.status(200).json(updatedUser) // STATUS CODE 200 = OK
+        return res.status(200).json({message: "Successfully updated user!"}) // STATUS CODE 200 = OK
     }
 
-    dataList.push(updatedUser)
+    userList.push(updatedUser)
 
-    return res.status(201).json(updatedUser) // STATUS CODE 201 = CREATED
+    return res.status(201).json({message: "As it couldn't find the user, created a new one"}) // STATUS CODE 201 = CREATED
 })
 
 // [x] Uma rota que atualiza apenas o endereço do usuário
 //PATCH
-app.patch("/data/:id", (req,res) =>{
+app.patch("/users/:id", (req,res) =>{
     const idUser = req.params.id
-    const onlyAddress = req.body
+    const newAddress = req.body
 
-    const thereIsTheUser = dataList.find(user => user.id == idUser)
+    const thereIsTheUser = userList.find(user => user.id == idUser)
     
-    onlyAddress[0] = dataList.address
-
     if(thereIsTheUser){
         const updatedUser ={
             ...thereIsTheUser,
-            ...onlyAddress
-        } //it ensures that all data will be subscribed
+            ...newAddress
+        } //it ensures that all users will be subscribed
 
-        dataList.map((user,index) =>{ // it ensures that the list will be updated with the new data
+        userList.map((user,index) =>{ // it ensures that the list will be updated with the new users
             if(user.id == idUser){
-                return dataList[index] = updatedUser
+                return userList[index] = updatedUser
             }
         })
 
-        return res.status(200).json(updatedUser) // STATUS CODE 200 = OK
+        return res.status(200).json({message: "Successfully updated user!", user: updatedUser}) // STATUS CODE 200 = OK
     }
-    return res.status(404).json({
-        message:"Can't find the user"
-    })
+    return res.status(404).json({message:`Can't find the user: ${idUser}`})
 })
 
 // [x] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
 //DELETE
-app.delete("/data/:id", (req, res) =>{
+app.delete("/users/:id", (req, res) =>{
     const idUser = req.params.id
 
-    const thereIsTheId = dataList.find((user) => user.id == idUser)
+    const thereIsTheUser = userList.find((user) => user.id == idUser)
 
-     if (thereIsTheId){
-
-        dataList.map((user, index) =>{
+     if (thereIsTheUser){
+        userList.map((user, index) =>{
             if(user.id == idUser) {
-                return dataList.splice(index,1)
+                return userList.splice(index,1)
             }
             })
-        return res.status(200).json(dataList) //STATUS CODE 200 = OK
+        return res.status(200).json({
+            message: "Successfully deleted user!", 
+            user: thereIsTheUser
+        }) //STATUS CODE 200 = OK
     }
-    return res.status(404).json({ //STATUS CODE 404 = NOT FOUND
-        message:"Can't find the user"
-    })
+    return res.status(404).json({message:`Can't find the user: ${idUser}`})//STATUS CODE 404 = NOT FOUND
 })
 
 // [x] Usar corretamente os retornos com os respectivos status codes!
