7fcd77005cdd27b185c7f28dc3cc30bab07ddbd1
judy <judysantos169@gmail.com>
2023-02-04 19:38:39 -0300

    updates

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,56 +1,50 @@
+
+// o express é um framework do node
+// já o require é usado pra importar módulos(para nodejs)
 const express = require("express")
 const app = express()
+//local onde está o banco de dados
 const listUsers = require("./model/usuarios.json")
 const numPort= 3004
-
-app.use(express.json())
-
-/*
+/*app.use () usado para Montar a função middleware
+ ou montar em um caminho especificado,
+ a função middleware é executada quando o caminho base corresponde./*
+*/
+ app.use(express.json())
  
-200 - OK
-201 - CREATED
-202 - ACCEPTED
-204 - NO CONTENT ( nao aceita corpo da requisição)
-404 - NOT FOUND
- */
-
 //- [1] Uma rota que atualiza todos os dados de cadastro de um usuário e se não for encontrado cria um novo na lista
-
-//ese código tá funcionando ! Ele atualiza o aniversario do usaurio
-//precisa  de uma requisição  
-app.put("/usuario/:id", (req, res)=>{
-    const birthUser = req.params.birth_date
+  
+app.put("/updateAll/:id", (req, res)=>{
+    //const IdUser = req.params.id
     const upgradeUser = req.body
 
-    const showUser = listUsers.find(user => user.birth_date == birthUser)
+    const showUser = listUsers.find(IdUser => IdUser.id == IdUser)
         if(showUser){
            
             return res.status(200).json(showUser)
         }
         listUsers.push(upgradeUser)
 
-     return res.status(201).json(upgradeUser)
+     return res.status(202).json(upgradeUser)
 })
-// poderia usar mais  campos pra atualizar , mas to com preguiça 
 
 //- [2] Uma rota que atualiza apenas o endereço do usuário
-app.patch("/usuario/:id", (req, res)=>{
-    const idUser = req.params.id
+app.patch("/updateAddress/:id", (req, res)=>{
+    const CityUser = req.params.id
     const newAddress = req.body
 
-    const showAdress = listUsers.find(user => idUser.id == idUser)
-        if(showAdress){
-           
-            //return res.status(200).json(showAdress)
+    const showAdress = listUsers.find(user => CityUser.id == CityUser)
+        if(showAdress){ 
+            return res.status(400).json({message:" não é possivel fazer tal alteração"})
         }
         listUsers.push(newAddress)
 
      return res.status(202).json(newAddress)
+        
 })
 
-
 //- [3] Uma rota que ao receber um ID de usuário , consegue deletar ele da lista de usuários.
-app.delete("/usuario/:id",(req, res)=>{
+app.delete("/deleteUser/:id",(req, res)=>{
     const deleteId = req.params.id
 
     const showId = listUsers.find((IdUser)=> IdUser.id == deleteId)
@@ -61,7 +55,7 @@ app.delete("/usuario/:id",(req, res)=>{
                 return listUsers.splice(index, 1)
             }
         })
-        return res.status(200).json(deleteId)
+        return res.status(200).json({message : `O usuario com o id ${deleteId}, foi deletato como sucesso`})
     }
 return res.status(404).json({
     message: "Usuario não encontrado"
@@ -69,8 +63,6 @@ return res.status(404).json({
 
  })
     
-
-
 app.listen(numPort, ()=>{
     console.log(`A aplicação está funcionando na porta ${numPort}`)
 })
