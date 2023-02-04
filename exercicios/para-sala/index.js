2411882e80c6f4d9b03f7c10c5053e7dd3d5b470
judy <judysantos169@gmail.com>
2023-02-04 19:42:15 -0300

    updates

--- exercicios/para-sala/index.js
+++ exercicios/para-sala/index.js
@@ -1,22 +1,46 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 const listaDeTarefas = require("./model/todo-list.json")
-const port = 3000
+const port = 3007
 
 app.use(express.json())
 
-/**
- * STATUS CODES:
- * 200 - OK
- * 201 - CREATED
- * 202 - ACCEPTED
- * 204 - NO CONTENT ( nao aceita corpo da requisição)
- * 
- * 404 - NOT FOUND
- */
-
 //TODO: [X] criaremos uma rota utilizando o método PUT que atualiza um registro da lista de tarefas e caso não encontre, cria o item. - DONE
 app.put("/tarefas/:id",(req, res)=>{
+    const idTarefa = req.params.id
+    const body = req.body
+
+    const novaLista = listaDeTarefas.map((tarefa)=>{
+        if(tarefa.id == idTarefa){
+            return tarefa = body
+        }
+        return tarefa
+    })
+    res.status(202).json(novaLista) 
+})
+
+//TODO: [X] vamos atualizar um registro na lista de tarefas utilizando o método PATCH - DONE
+app.patch("/tarefas/:id",(req, res)=>{
+    const IDtarefa = req.params.id
+    const novosCampos = req.body
+
+    const novaTarefa = listaDeTarefas.find(tarefa=> tarefa.id ==IDtarefa)
+    if(novaTarefa){
+        const tarefaAtaulizada={
+            ...novaTarefa,
+            ...novosCampos
+        }
+        return res.status(200).json(tarefaAtaulizada)
+    }
+    return res.status(404).json({message:"tarefa não encontrada"})
+     } )  
+       
+    //res.status(202).json(novaTarefa)//
+    //console.log(novaTarefa)//exibirá todos os itens com o due date modificado if(tarefa.id = IDtarefa)
+
+//exemplo 2 de put
+app.put("/tarefa/:id",(req, res)=>{
     const IDtarefa = req.params.id
     const tarefaAtualizada = req.body
 
@@ -37,38 +61,12 @@ app.put("/tarefas/:id",(req, res)=>{
     
 })
 
-//TODO: [X] vamos atualizar um registro na lista de tarefas utilizando o método PATCH - DONE
-
-app.patch("/tarefas/:id",(req, res)=>{
-    const IDtarefa = req.params.id
-    const novosCampos = req.body
-
-    const existeTarefa = listaDeTarefas.find(tarefa => tarefa.id == IDtarefa)
-
-    if(existeTarefa) {
-        const tarefaAtualizada = {
-            ...existeTarefa,
-            ...novosCampos
-        }
-        // Garante que a lista de tarefas vai ser atualizada com o novo registro
-        listaDeTarefas.map((tarefa, index)=>{
-            if(tarefa.id == IDtarefa){
-                return listaDeTarefas[index] = tarefaAtualizada
-            }
-        })
-
-        return res.status(200).json(tarefaAtualizada)
-    }
-    return res.status(404).json({message:"tarefa não foi encontrada"})
-})
-
 //TODO: [X] apagaremos uma tarefa da lista utilizando o método DELETE. - DONE
 app.delete("/tarefas/:id",(req, res)=>{
     const IDtarefa = req.params.id
 
     const existeTarefa = listaDeTarefas.find((tarefa) => tarefa.id == IDtarefa)
-    //False = 0, null , [] , {} , undefined , false
-    //True = {...}, 1 , True
+
     if(existeTarefa){
         listaDeTarefas.map((tarefa, index)=>{
             if(tarefa.id == IDtarefa){
@@ -80,11 +78,10 @@ app.delete("/tarefas/:id",(req, res)=>{
     }
 
     return res.status(404).json({
-        message:"Tarefa não foi encontrada"
+        message:`Tarefa com o id ${IDtarefa }não foi encontrada`
     })
 })
 
-
 app.listen(port,()=>{
-    console.log(`Api está rodando na porta ${port}`)
+    console.log(`Aplicação funcionando na porta ${port}`)
 })
